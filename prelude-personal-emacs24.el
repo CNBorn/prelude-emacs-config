0cc2b46dedc0d129e31a8721bda552ae932f04ae
Tyler Xing <CNBorn@hotmail.com>
2015-12-28 12:03:46 -0500

    clean up remaining helm config

--- prelude-personal-emacs24.el
+++ prelude-personal-emacs24.el
@@ -22,18 +22,8 @@
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 
-;; Helm, without using helm everywhere.
-(setq helm-split-window-default-side "below")
-(setq helm-split-window-in-side-p nil)
-;; shit helm can get sooooooo slow.
-;;(global-set-key (kbd "M-x") 'helm-M-x)
-;;(global-set-key (kbd "C-x b") 'helm-mini)
-;;(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
-
 ;; Projectile
 (setq projectile-completion-system 'grizzl)
-
-;;(helm-projectile-on)
 
 ;; Projectile ag-search
 (setq ag-arguments (list "--after=5" "--before=3" "--line-number" "--smart-case" "--nogroup" "--column" "--stats" "--"))
